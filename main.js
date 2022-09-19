6e710090410cda4aac183f7906aee34d792c6d0b
Your Name <alobryan2@gmail.com>
2022-09-19 18:50:19 -0400

    About 50% done with calculator probably less

---
+++ main.js
@@ -0,0 +1,53 @@
+//1. make it so the number buttons goes onto the display
+//1A. I need JS to find the class with the associated number 
+//1B. I need JS to then take that number and  
+let calcButtons = document.querySelectorAll('.input');
+console.log(calcButtons)
+let Calculation = document.querySelector('#display');
+
+for (let calcButton of calcButtons) {
+    //this makes it so javascript knows what button is press
+    calcButton.addEventListener('click', (event) => {
+        console.log(`the number: ${calcButton.innerText}`);
+        //making an array that can be manipulated
+        let displayArr = Calculation.innerText;
+        let limit = displayArr.length + 1;
+        console.log(limit);
+
+            switch (calcButton.id) {
+                case "0": Calculation.innerText = Calculation.innerText + 0; break;
+                case "1": Calculation.innerText = Calculation.innerText + 1; break;
+                case "2": Calculation.innerText = Calculation.innerText + 2; break;
+                case "3": Calculation.innerText = Calculation.innerText + 3; break;
+                case "4": Calculation.innerText = Calculation.innerText + 4; break;
+                case "5": Calculation.innerText = Calculation.innerText + 5; break;
+                case "6": Calculation.innerText = Calculation.innerText + 6; break;
+                case "7": Calculation.innerText = Calculation.innerText + 7; break;
+                case "8": Calculation.innerText = Calculation.innerText + 8; break;
+                case "9": Calculation.innerText = Calculation.innerText + 9; break;
+                case "+": Calculation.innerText = Calculation.innerText + "+"; break;
+                case "-": Calculation.innerText = Calculation.innerText + "-"; break;
+                case "/": Calculation.innerText = Calculation.innerText + "/"; break;
+                case ".": Calculation.innerText = Calculation.innerText + "."; break;
+                case "*": Calculation.innerText = Calculation.innerText + "x"; break;
+            }
+
+
+
+
+        // if (calcButton.id === "1")
+        //     {
+        //         Calculation.innerText = Calculation.innerText + 1;
+        //     }
+        // else if (calcButton.id === "2")
+        //     {
+        //         Calculation.innerText = Calculation.innerText + 2;
+        //     }
+
+    })
+
+}
+
+
+//2. make it so multiple numbers can be added until the = is pressed
+//3. when equal is pressed clear display and display the output 
