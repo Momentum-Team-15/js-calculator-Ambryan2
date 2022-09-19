993b1947965468821e1d7ddf5b13beb49ee17cc4
Your Name <alobryan2@gmail.com>
2022-09-19 19:23:36 -0400

    more things added

--- main.js
+++ main.js
@@ -2,36 +2,37 @@
 //1A. I need JS to find the class with the associated number 
 //1B. I need JS to then take that number and  
 let calcButtons = document.querySelectorAll('.input');
-console.log(calcButtons)
+// console.log(calcButtons)
 let Calculation = document.querySelector('#display');
 
 for (let calcButton of calcButtons) {
     //this makes it so javascript knows what button is press
     calcButton.addEventListener('click', (event) => {
         console.log(`the number: ${calcButton.innerText}`);
+
+        //This makes sure the numbers that are pressed are displayed in #display
+        switch (calcButton.id) {
+            case "0": Calculation.innerText = Calculation.innerText + 0; break;
+            case "1": Calculation.innerText = Calculation.innerText + 1; break;
+            case "2": Calculation.innerText = Calculation.innerText + 2; break;
+            case "3": Calculation.innerText = Calculation.innerText + 3; break;
+            case "4": Calculation.innerText = Calculation.innerText + 4; break;
+            case "5": Calculation.innerText = Calculation.innerText + 5; break;
+            case "6": Calculation.innerText = Calculation.innerText + 6; break;
+            case "7": Calculation.innerText = Calculation.innerText + 7; break;
+            case "8": Calculation.innerText = Calculation.innerText + 8; break;
+            case "9": Calculation.innerText = Calculation.innerText + 9; break;
+            case "+": Calculation.innerText = Calculation.innerText + "+"; break;
+            case "-": Calculation.innerText = Calculation.innerText + "-"; break;
+            case "/": Calculation.innerText = Calculation.innerText + "/"; break;
+            case ".": Calculation.innerText = Calculation.innerText + "."; break;
+            case "*": Calculation.innerText = Calculation.innerText + "*"; break;
+        }
+        //make sure that numbers pressed are saved to be manipulated with math expressions
         //making an array that can be manipulated
         let displayArr = Calculation.innerText;
-        let limit = displayArr.length + 1;
-        console.log(limit);
-
-            switch (calcButton.id) {
-                case "0": Calculation.innerText = Calculation.innerText + 0; break;
-                case "1": Calculation.innerText = Calculation.innerText + 1; break;
-                case "2": Calculation.innerText = Calculation.innerText + 2; break;
-                case "3": Calculation.innerText = Calculation.innerText + 3; break;
-                case "4": Calculation.innerText = Calculation.innerText + 4; break;
-                case "5": Calculation.innerText = Calculation.innerText + 5; break;
-                case "6": Calculation.innerText = Calculation.innerText + 6; break;
-                case "7": Calculation.innerText = Calculation.innerText + 7; break;
-                case "8": Calculation.innerText = Calculation.innerText + 8; break;
-                case "9": Calculation.innerText = Calculation.innerText + 9; break;
-                case "+": Calculation.innerText = Calculation.innerText + "+"; break;
-                case "-": Calculation.innerText = Calculation.innerText + "-"; break;
-                case "/": Calculation.innerText = Calculation.innerText + "/"; break;
-                case ".": Calculation.innerText = Calculation.innerText + "."; break;
-                case "*": Calculation.innerText = Calculation.innerText + "x"; break;
-            }
-
+        //since array doesn't read the special characters I plan on having calculator take everything before symbol and do the symbol act to everything after
+        // console.log(eval(displayArr))
 
 
 
