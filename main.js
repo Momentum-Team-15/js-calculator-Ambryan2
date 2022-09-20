542975df0abda2b5e874580768f39b4ec95102d4
Your Name <alobryan2@gmail.com>
2022-09-20 09:32:54 -0400

    comments and organization

--- main.js
+++ main.js
@@ -12,43 +12,43 @@ for (let calcButton of calcButtons) {
         let displayArr = Calculation.innerText;
 
         //This makes sure the numbers that are pressed are displayed in #display
-        if (displayArr.length <= 18){
-        switch (calcButton.id) {
-            case "0": Calculation.innerText = Calculation.innerText + 0; break;
-            case "1": Calculation.innerText = Calculation.innerText + 1; break;
-            case "2": Calculation.innerText = Calculation.innerText + 2; break;
-            case "3": Calculation.innerText = Calculation.innerText + 3; break;
-            case "4": Calculation.innerText = Calculation.innerText + 4; break;
-            case "5": Calculation.innerText = Calculation.innerText + 5; break;
-            case "6": Calculation.innerText = Calculation.innerText + 6; break;
-            case "7": Calculation.innerText = Calculation.innerText + 7; break;
-            case "8": Calculation.innerText = Calculation.innerText + 8; break;
-            case "9": Calculation.innerText = Calculation.innerText + 9; break;
-            case "+": Calculation.innerText = Calculation.innerText + "+"; break;
-            case "-": Calculation.innerText = Calculation.innerText + "-"; break;
-            case "/": Calculation.innerText = Calculation.innerText + "/"; break;
-            case ".": Calculation.innerText = Calculation.innerText + "."; break;
-            case "*": Calculation.innerText = Calculation.innerText + "*"; break;
-            case "C": Calculation.innerText = ' ';break;
-            case "=":
-                let answer = eval(displayArr);
-                Calculation.innerText = ' ';
-                Calculation.innerText = Calculation.innerText + answer;
-                break;
-        }
-        }
-        else {
-            switch (calcButton.id){
-            case "C": Calculation.innerText = ' ';break;
-            case "=":
-                let answer = eval(displayArr);
-                Calculation.innerText = ' ';
-                Calculation.innerText = Calculation.innerText + answer;
-                break;
+        if (displayArr.length <= 18) {
+            switch (calcButton.id) {
+                case "0": Calculation.innerText = Calculation.innerText + 0; break;
+                case "1": Calculation.innerText = Calculation.innerText + 1; break;
+                case "2": Calculation.innerText = Calculation.innerText + 2; break;
+                case "3": Calculation.innerText = Calculation.innerText + 3; break;
+                case "4": Calculation.innerText = Calculation.innerText + 4; break;
+                case "5": Calculation.innerText = Calculation.innerText + 5; break;
+                case "6": Calculation.innerText = Calculation.innerText + 6; break;
+                case "7": Calculation.innerText = Calculation.innerText + 7; break;
+                case "8": Calculation.innerText = Calculation.innerText + 8; break;
+                case "9": Calculation.innerText = Calculation.innerText + 9; break;
+                case "+": Calculation.innerText = Calculation.innerText + "+"; break;
+                case "-": Calculation.innerText = Calculation.innerText + "-"; break;
+                case "/": Calculation.innerText = Calculation.innerText + "/"; break;
+                case ".": Calculation.innerText = Calculation.innerText + "."; break;
+                case "*": Calculation.innerText = Calculation.innerText + "*"; break;
+                case "C": Calculation.innerText = ' '; break;
+                case "=":
+                    let answer = eval(displayArr);
+                    Calculation.innerText = ' ';
+                    Calculation.innerText = Calculation.innerText + answer;
+                    break;
             }
         }
-    
-        console.log(Calculation.innerText)
+        //in rare case the string goes one over due to .length it will still do these buttons
+        else {
+            switch (calcButton.id) {
+                case "C": Calculation.innerText = ' '; break;
+                case "=":
+                    let answer = eval(displayArr);
+                    Calculation.innerText = ' ';
+                    Calculation.innerText = Calculation.innerText + answer;
+                    break;
+            }
+        }
+
 
     })
 
